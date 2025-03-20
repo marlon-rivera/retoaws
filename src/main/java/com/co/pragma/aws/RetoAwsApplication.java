a82015287c2e686732e3a570116a54395713320c
marlon-rivera <marlon.rivera@uptc.edu.co>
2025-03-20 18:49:00 -0500

    Project structure

---
+++ src/main/java/com/co/pragma/aws/RetoAwsApplication.java
@@ -0,0 +1,13 @@
+package com.co.pragma.aws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RetoAwsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RetoAwsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/co/pragma/aws/RetoAwsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.co.pragma.aws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RetoAwsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
